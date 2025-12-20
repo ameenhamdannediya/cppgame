17a8be52a696c7bdea41b83a53392a1ca8c56839
ameenhamdannediya <ameentheprogramer@gmail.com>
2025-12-20 01:03:27 -0800

    added basic bullet and firing system

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,24 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <math.h>
 
+sf::Vector2f normalize(sf::Vector2f vector) {
+	float magnitude = std::sqrt((vector.x * vector.x) + (vector.y * vector.y));
+	sf::Vector2f normalised;
+	normalised.x = vector.x / magnitude;
+	normalised.y = vector.y / magnitude;
 
-
+	return normalised ;
+}
 
 
 int main() {
-	std::vector<std::string> games ;
-	games.push_back("gta4");
-	games.push_back("gta5");
-	games.push_back("gta san andreas");
-	games.push_back("far cry");
-
-	for (int i = 0; i < games.size(); i++) {
-		std::cout << games[i] << std::endl;
-	
-	}
-
-
 
 
 	sf::ContextSettings settings;
@@ -27,19 +22,8 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "project1");
 	window.setKeyRepeatEnabled(false);
 
-
-	//sf::CircleShape shape(40.0f, 8);
-	//shape.setFillColor(sf::Color::Red);
-	//shape.setPosition(sf::Vector2f(320, 100));
-	//shape.setOutlineThickness(10);
-	//shape.setOutlineColor(sf::Color::Blue);
-
-	//sf::RectangleShape r1(sf::Vector2f(20.0f,200.0f));
-	////r1.setOrigin(sf::Vector2f(r1.getGeometricCenter()));
-	//r1.setOrigin(sf::Vector2f(r1.getSize()/2.0f));
-	//r1.setPosition(sf::Vector2f(360 ,290));
-	//r1.setRotation(sf::degrees(45.0f));
-
+	std::vector<sf::RectangleShape >bullet;
+	float bulletspeed = 1.0f;
 
 	int Xindex = 0;
 	int Yindex = 0;
@@ -49,8 +33,8 @@ int main() {
 		std::cout << "player head faild to load" << std::endl;
 
 	}
-		sf::Sprite playersheadleatherprite(playerheadleather);
-		playersheadleatherprite.setTextureRect(sf::IntRect({ {Xindex * 64, Yindex * 64} , {64, 64} }));
+	sf::Sprite playersheadleatherprite(playerheadleather);
+	playersheadleatherprite.setTextureRect(sf::IntRect({ {Xindex * 64, Yindex * 64} , {64, 64} }));
 
 
 	sf::Texture playertexture;
@@ -62,17 +46,16 @@ int main() {
 	sf::Sprite playersprite(playertexture);
 	playersprite.setTextureRect(sf::IntRect({ {Xindex * 64, Yindex * 64} , {64, 64} }));
 
-
-
-	int enemyXindex = 2;
-	int enemyYindex = 4			;
-
 	if (!playertexture.loadFromFile("assets/player/textures/playerspritesheet.png")) {
 		std::cout << "player faild to load" << std::endl;
 	}
-	std::cout << "enymy loading player" << std::endl;
+	std::cout << "loading player" << std::endl;
 	sf::Sprite enemysprite(playertexture);
-	enemysprite.setTextureRect(sf::IntRect({ {enemyXindex * 64, enemyYindex * 64} , {64, 64} }));
+	enemysprite.setTextureRect(sf::IntRect({ {Xindex * 64, Yindex * 64} , {64, 64} }));
+	enemysprite.setPosition(sf::Vector2f(100, 100));
+
+
+	
 
 
 
@@ -87,16 +70,39 @@ int main() {
 
 
 
-
-
-
+	sf::Vector2f bulletdirection;
 
 
 	while (window.isOpen()) {
+
 		while (std::optional event = window.pollEvent()) {
+
 			if (event->is < sf::Event::Closed>())
 				window.close();
+
 		}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+		{
+			bullet.push_back(sf::RectangleShape(sf::Vector2f(25,10)));
+			int i = bullet.size() - 1;
+			bullet[i].setPosition(playersprite.getPosition());
+
+
+
+		//	//sf::Vector2i cursorposition = sf::Mouse::getPosition(window);
+		//	//sf::Vector2f worldPos = window.mapPixelToCoords(cursorposition);
+		//	//sf::Vector2f bulletdirection = worldPos - bullet.getPosition();
+		//	//bullet.setPosition(bullet.getPosition() + bulletdirection* bulletspeed);
+
+		}
+		for (size_t i = 0; i < bullet.size(); i++)
+		{
+			bulletdirection = enemysprite.getPosition() - bullet[i].getPosition();
+			bulletdirection = normalize(bulletdirection);
+			bullet[i].setPosition(bullet[i].getPosition() + bulletdirection * bulletspeed);
+
+		}
+
 
 
 		sf::Vector2f PrePOsition = playersprite.getPosition();
@@ -122,13 +128,23 @@ int main() {
 
 
 		window.clear(sf::Color::Black);
-		window.draw(enemysprite);
+		for (size_t i = 0; i < bullet.size(); i++)
+		{
+			window.draw(bullet[i]);
 
+		}
+
+		window.draw(enemysprite);
 		window.draw(playersprite);
 		window.draw(playersheadleatherprite);
-		
+
 		window.display();
 	};
 
 	return 0;
 }
+
+
+
+
+
